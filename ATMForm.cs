30ebc899fae2c59fde541877202999072cef555f
Aleksander Barczak <alex.bar074@gmail.com>
2024-03-12 11:38:14 +0000

    creation and closing of new forms for ATMs is consistent and safe

---
+++ ATMForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AC22005Assignment3
+{
+    public partial class ATMForm : Form
+    {
+        public delegate void PrintDelegate(string output);
+
+        private Main mainForm;
+
+        public ATMForm(Main mainForm)
+        {
+            InitializeComponent();
+            this.mainForm = mainForm;
+            mainForm.ATMList.AddLast(this);
+            mainForm.BeginInvoke(new PrintDelegate(mainForm.printToOutputWindow), Thread.CurrentThread.ManagedThreadId.ToString());
+            Application.Run(this);
+        }
+    }
+}
--- MainForm.cs
+++ MainForm.cs
@@ -4,7 +4,10 @@ namespace AC22005Assignment3
 {
     public partial class Main : Form
     {
-        Queue<string> OutputWindowBuffer = new Queue<string>();
+        public delegate void CloseDelegate();
+
+        public LinkedList<ATMForm> ATMList = new LinkedList<ATMForm>();
+        private Queue<string> OutputWindowBuffer = new Queue<string>();
         const int OutputWindowCharWidth = 42;
         const int OutputWindowCharHeight = 14;
 
@@ -36,11 +39,26 @@ namespace AC22005Assignment3
         public Main()
         {
             InitializeComponent();
+            this.FormClosing += new FormClosingEventHandler(Main_FormClosing);
         }
 
         private void btn_CreateATM_Click(object sender, EventArgs e)
         {
+            Thread newATMThread = new Thread(new ThreadStart(CreateATM));
+            newATMThread.Start();
+        }
 
+        private void CreateATM()
+        {
+            ATMForm newATM = new ATMForm(this);
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (ATMForm ATM in ATMList)
+            {
+                ATM.BeginInvoke(new CloseDelegate(ATM.Close));
+            }
         }
     }
 }
